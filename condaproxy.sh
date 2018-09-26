2d42c2dcd466d71214bc8f59db9befb68c75894f
Johan van der Meer <johanvandermeer@gmail.com>
2018-09-26 15:55:39 +1000

    added new task

--- condaproxy.sh
+++ condaproxy.sh
@@ -1,2 +1,8 @@
+# conda uses the https proxy.
+
+# id:pw can be your QIMR id and PW
+# address is webproxy.qimr.edu.au or webproxy.adqimr.ad.lan, it doesn't matter -- the IP is 10.10.2.71 for both
+# port is 8080
+
 conda config --set proxy_servers.http http://id:pw@address:port
 conda config --set proxy_servers.https https://id:pw@address:port
